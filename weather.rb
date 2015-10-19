d04f8d22bc8d567688578786a1b1d4c15e6469bf
natalya.patrikeeva <natalya.patrikeeva@gmail.com>
2015-10-18 21:39:48 -0500

    first commit

---
+++ weather.rb
@@ -0,0 +1,35 @@
+require 'yahoo_weatherman'
+
+
+
+def determine_weather(location)
+  weather = Weatherman::Client.new
+  response = weather.lookup_by_location(location)
+  forecast = response.description
+  # puts forecast
+  split = forecast.split(' />')
+
+  for i in 5..9
+    loop_split = split[i]
+    weekday = loop_split.split('. ')[0]
+    high_low = loop_split.split('. ')[1]
+
+    if (i == 5)
+      puts "Today is going to be #{weekday.split('- ')[1]} \
+with a low of #{high_low.split(' ')[3].chop.chop.chop} and a high of #{high_low.split(' ')[1]} \
+degrees Celsius."
+    elsif (i == 6)
+      puts "Tomorrow is going to be #{weekday.split('- ')[1]} \
+with a low of #{high_low.split(' ')[3].chop.chop.chop} and a high of #{high_low.split(' ')[1]} \
+degrees Celsius."
+    else
+      puts "#{weekday.split('- ')[0].chop} is going to be #{weekday.split('- ')[1]} \
+with a low of #{high_low.split(' ')[3].chop.chop.chop} and a high of #{high_low.split(' ')[1]} \
+degrees Celsius."
+    end
+  end
+end
+
+puts "Please enter your location: "
+location = gets
+determine_weather(location)
